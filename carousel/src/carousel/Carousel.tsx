6f1c207ddd86c25d91b97f74dc40f099f881cb81
Patrick Smith <patrick@burntcaramel.com>
2018-06-08 17:35:39 +1000

    Add animating next

--- carousel/src/carousel/Carousel.tsx
+++ carousel/src/carousel/Carousel.tsx
@@ -22,11 +22,14 @@ class CarouselTrack extends React.Component<ITrackProps> {
   public render() {
     const { items, offset } = this.props
     return <div style={{ display: 'flex', flexDirection: 'row', width: 50000, transform: `translateX(${offset * -100}vw)` }}>
-      {
-        items.map((item) => (
-          <CarouselSlide item={ item } />
-        ))
-      }
+      <>
+        {
+          items.map((item, index) => (
+            <CarouselSlide key={index} item={ item } />
+          ))
+        }
+        {!!items[0] && <CarouselSlide key='extra' item={ items[0] } /> }
+      </>
     </div>
   }
 }
@@ -45,6 +48,12 @@ interface IFrameState {
   offset: number
 }
 
+// async function nextFrame(): Promise<number> {
+//   return new Promise((resolve) => {
+//     requestAnimationFrame(resolve)
+//   })
+// }
+
 class CarouselFrame extends React.Component<IFrameProps, IFrameState> {
   state = {
     offset: 0
@@ -54,20 +63,40 @@ class CarouselFrame extends React.Component<IFrameProps, IFrameState> {
     return this.props.items.length
   }
 
+  private changeOffsetBy(change: number) {
+    let startOffset = this.state.offset
+    const endOffset = startOffset + change
+    let currentOffset = startOffset
+    const frameChange = change / 20.0
+
+    const updateState = () => {
+      currentOffset = Math.min(currentOffset + frameChange, endOffset)
+      // const clampedOffset = (currentOffset + this.maxOffset) % this.maxOffset
+      this.setState({ offset: currentOffset })
+
+      if (currentOffset < endOffset) {
+        requestAnimationFrame(updateState)
+      }
+    }
+
+    requestAnimationFrame(updateState)
+  }
+
   private onGoPrevious = () => {
     this.setState(({ offset }) => ({ offset: (offset + this.maxOffset - 1.0) % this.maxOffset }))
   }
 
   private onGoNext = () => {
-    this.setState(({ offset }) => ({ offset: (offset + 1.0) % this.maxOffset }))
+    this.changeOffsetBy(1.0)
   }
 
   public render() {
     const { items, renderNav } = this.props
     const { offset } = this.state
+    const clampedOffset = (offset + this.maxOffset) % this.maxOffset
     return <div>
       <div style={{ overflow: 'hidden' }}>
-        <CarouselTrack items={items} offset={offset} />
+        <CarouselTrack items={items} offset={clampedOffset} />
       </div>
       { renderNav({ goPrevious: this.onGoPrevious, goNext: this.onGoNext }) }
     </div>
