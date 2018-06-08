2a265261c9689964e734b57ca0e8d146e22b7f67
Patrick Smith <patrick@burntcaramel.com>
2018-06-08 17:18:17 +1000

    Add navigating between slides

--- carousel/src/App.tsx
+++ carousel/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import './App.css';
-import { CarouselInner } from './carousel/Carousel'
+import { CarouselFrame } from './carousel/Carousel'
 import { ICarouselItem } from './carousel/types'
 
 const items: ICarouselItem[] = [
@@ -11,6 +11,10 @@ const items: ICarouselItem[] = [
   {
     description: '200x200',
     imageURL: 'https://placehold.it/200x200'
+  },
+  {
+    description: '300x300',
+    imageURL: 'https://placehold.it/300x300'
   }
 ]
 
@@ -18,9 +22,19 @@ class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <CarouselInner items={ items } />
+        <CarouselFrame
+          items={ items }
+          renderNav={this.renderNav}
+        />
       </div>
     );
+  }
+
+  private renderNav({ goPrevious, goNext }: { goPrevious: () => {}, goNext: () => {} }) {
+    return (<>
+      <button onClick={goPrevious}>Previous</button>
+      <button onClick={goNext}>Next</button>
+    </>)
   }
 }
 
--- carousel/src/carousel/Carousel.tsx
+++ carousel/src/carousel/Carousel.tsx
@@ -1,27 +1,80 @@
 import * as React from 'react'
 import * as types from './types'
 
-interface IProps {
+interface ISlideProps {
+  item: types.ICarouselItem
+}
+
+function CarouselSlide({ item }: ISlideProps): React.ReactElement<any> {
+  return (
+    <div style={{ minWidth: '100vw', height: '100%' }}>
+      <img src={ item.imageURL } alt={ item.description } />
+    </div>
+  )
+}
+
+interface ITrackProps {
   items: types.ICarouselItem[]
+  offset: number
 }
 
-interface IState {
-  currentIndex: number | null
-}
-
-class CarouselInner extends React.Component<IProps, IState> {
+class CarouselTrack extends React.Component<ITrackProps> {
   public render() {
-    const { items } = this.props
-    return <>
+    const { items, offset } = this.props
+    return <div style={{ display: 'flex', flexDirection: 'row', width: 50000, transform: `translateX(${offset * -100}vw)` }}>
       {
         items.map((item) => (
-          <img src={ item.imageURL } alt={ item.description } />
+          <CarouselSlide item={ item } />
         ))
       }
-    </>
+    </div>
+  }
+}
+
+interface IRenderNavArgs {
+  goPrevious: () => void
+  goNext: () => void
+}
+
+interface IFrameProps {
+  items: types.ICarouselItem[]
+  renderNav: (args: IRenderNavArgs) => React.ReactElement<any>
+}
+
+interface IFrameState {
+  offset: number
+}
+
+class CarouselFrame extends React.Component<IFrameProps, IFrameState> {
+  state = {
+    offset: 0
+  }
+
+  get maxOffset() {
+    return this.props.items.length
+  }
+
+  private onGoPrevious = () => {
+    this.setState(({ offset }) => ({ offset: (offset + this.maxOffset - 1.0) % this.maxOffset }))
+  }
+
+  private onGoNext = () => {
+    this.setState(({ offset }) => ({ offset: (offset + 1.0) % this.maxOffset }))
+  }
+
+  public render() {
+    const { items, renderNav } = this.props
+    const { offset } = this.state
+    return <div>
+      <div style={{ overflow: 'hidden' }}>
+        <CarouselTrack items={items} offset={offset} />
+      </div>
+      { renderNav({ goPrevious: this.onGoPrevious, goNext: this.onGoNext }) }
+    </div>
   }
 }
 
 export {
-  CarouselInner
+  CarouselTrack,
+  CarouselFrame
 }
